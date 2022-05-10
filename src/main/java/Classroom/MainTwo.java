1ec4651db8148c9d1dc6b9816829331cdafc12df
7Zanee <62302626+7Zanee@users.noreply.github.com>
2022-05-10 21:53:16 +0300

    add extra examples

---
+++ src/main/java/Classroom/MainTwo.java
@@ -0,0 +1,17 @@
+package Classroom;
+
+public class MainTwo {
+    public static void main(String[] args) {
+        //code here
+        String vards = "Zane";
+        String uzvards = "Ergle";
+        int vecums = 22;
+        boolean isPresent = true;
+
+        System.out.println("Vārds: " +vards + "\n" + "Uzvārds: " +uzvards + "\n"
+        + "Vecums: " +vecums + "\n" + "Vai bija nodarbībā? " +isPresent + "\n");
+
+        String teikums = String.format("Vārds: %s \n Uzvārds %s \n Vecums %s \n isPresent %s",vards, uzvards, vecums, isPresent);
+        System.out.println(teikums);
+    }
+}
